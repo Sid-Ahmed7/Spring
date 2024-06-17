a1e440a9b870360f3971fab382c404dab2604cb6
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-06-17 11:27:38 +0200

    Update method getDoctor

--- src/main/java/com/doctor/doctorWho/controller/DoctorController.java
+++ src/main/java/com/doctor/doctorWho/controller/DoctorController.java
@@ -3,13 +3,13 @@ package com.doctor.doctorWho.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DoctorController {
 
-    @GetMapping("/doctor/{id}")
+    @GetMapping("/doctor/{doctorId}")
     @ResponseBody
     public String getDoctor(@PathVariable int doctorId) {
         switch (doctorId) {
