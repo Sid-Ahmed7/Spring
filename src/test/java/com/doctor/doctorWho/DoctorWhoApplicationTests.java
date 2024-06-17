e58227f16312679c32cf78548fb8cfd43fe32df7
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-06-17 10:31:12 +0200

    initial commit

---
+++ src/test/java/com/doctor/doctorWho/DoctorWhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctor.doctorWho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorWhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
